7ddfd65d80c52718a841a4efa8749d93dffcc377
家子智成 <>
2020-07-23 22:40:38 +0900

    クリア判定実装

---
+++ Assets/0_MyAssets/Scripts/Game/BallController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Goal")) return;
+        if (Variables.screenState != ScreenState.Game) return;
+        Variables.screenState = ScreenState.Clear;
+        Debug.Log(Variables.screenState);
+    }
+}
